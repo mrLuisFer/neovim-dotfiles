23989c620beada68377cb75fea9eb5ead5122bf8
mrLuisFer <mrluisfeer@gmail.com>
2021-10-01 23:13:44 -0500

    [feat] add lsp languages

--- lua/lua-completion-config.lua
+++ lua/lua-completion-config.lua
@@ -23,4 +23,3 @@ require'lspconfig'.tsserver.setup{on_attach=require'completion'.on_attach}
 require'lspconfig'.vuels.setup{on_attach=require'completion'.on_attach}
 
 require'lspconfig'.yamlls.setup{on_attach=require'completion'.on_attach}
-
