9861ddbdd4e4ccaf1fdca9c2db245104f7c1496a
mrLuisFer <mrluisfeer@gmail.com>
2021-11-05 14:49:10 -0600

    remove cmd for set colorscheme

--- lua/themes/tokyodark-config.lua
+++ lua/themes/tokyodark-config.lua
@@ -4,5 +4,4 @@ g.tokyodark_transparent_background = false
 g.tokyodark_enable_italic_comment = true
 g.tokyodark_enable_italic = true
 g.tokyodark_color_gamma = "1.0"
-vim.cmd("colorscheme tokyodark")
 
