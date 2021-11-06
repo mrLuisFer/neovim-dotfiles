b20d5e954ba22b96da3c5ccc8b3dad38b7625716
mrLuisFer <mrluisfeer@gmail.com>
2021-11-06 14:46:50 -0600

    update new completion with cmp and tabnine
    https://github.com/topics/nvim-cmp

--- init.vim
+++ init.vim
@@ -124,8 +124,8 @@ lua require('plugins/lspsaga-config')
 lua require('plugins/impatient-config')
 lua require('plugins/galaxyline-config')
 lua require('plugins/vim_go_config')
-lua require('plugins/nvim_compe_config')
 lua require('plugins/rainbow')
+lua require('plugins/completion-config')
 lua require('plugins/rainbow')
 
 " THEMES:
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -82,7 +82,6 @@ return require('packer').startup(function()
   use 'glepnir/dashboard-nvim'
   use 'akinsho/nvim-bufferline.lua'
 	use 'editorconfig/editorconfig-vim'
-  use 'hrsh7th/nvim-compe'
   use 'williamboman/nvim-lsp-installer'
 	use 'antoinemadec/FixCursorHold.nvim'
   -- INTEGRATIONS: ------------------------------
@@ -102,4 +101,21 @@ return require('packer').startup(function()
     run = ':GoUpdateBinaries'
   }
 
+  use 'neovim/nvim-lspconfig'
+  -- Copletion
+  use {
+    "hrsh7th/nvim-cmp",
+   requires = {
+      "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
+      'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
+      'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
+      'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
+    }
+  }
+  use {
+    'tzachar/cmp-tabnine',
+    run = './install.sh',
+    requires = 'hrsh7th/nvim-cmp'
+  }
+  use 'onsails/lspkind-nvim'
 end)
